5bd00074e03bf6544267b2a57db9ca4ace1199b7
41406133 <41406133@089705-P33S08.macklabs.br>
2015-04-17 10:32:53 -0300

    Semana oito com exemplo

---
+++ Mackenzie_LP1/src/semana8/ExemploArquivos.java
@@ -0,0 +1,41 @@
+﻿/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemploarquivos;
+
+import java.io.*;
+
+/**
+ *
+ * @author 1132587
+ */
+public class ExemploArquivos {
+
+    /**
+     * copia arquivos
+     */
+    public static void main(String[] args) {
+        try{
+           BufferedReader r=new BufferedReader(new FileReader("entrada.txt")); // leitura
+           BufferedWriter w=new BufferedWriter(new FileWriter("saida.txt")); // gravação
+           // O que acontece se os arqiuvos nao puderam ser abertos ?
+           String linha = r.readLine();  // quantidade de dados a serem lidos
+           
+           w.write(linha+'\n');   // grava quantidade no arquivo de saída
+           int q = Integer.parseInt(linha);
+                   
+           for(int i=0;i<q;i++){
+              linha = r.readLine();
+              w.write(linha.toUpperCase()+'\n');
+           }
+           r.close();   // fecha arquivo de entrada
+           w.flush();  // força escrita final de dados no arquivo de saída
+           w.close(); // fecha arquivo de saída
+        }
+        catch(IOException | NumberFormatException e){
+            System.exit(-1);
+        } 
+
+    }
+}
